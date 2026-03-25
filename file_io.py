6894da11e1f188022329d5b9a404cd27c0e7d0c5
Vadim Varshavskiy <vadim_varshavskiy@yahoo.com>
2026-03-25 14:11:40 +0300

    Implemented reading the greeting from a text file

--- file_io.py
+++ file_io.py
@@ -2,8 +2,8 @@ FILE_NAME="input.txt"
 
 def read_greeting():
     greeting_file=open(FILE_NAME, 'r')
-    content=greeting_file.read()
-    return content
+    _content=greeting_file.read()
+    return _content
 
 if __name__=='__main__':
     print(read_greeting())
