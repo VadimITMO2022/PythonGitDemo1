c4470101868734dd4ee4503d968663aa8d5757ef
Vadim Varshavskiy <vadim_varshavskiy@yahoo.com>
2026-03-25 14:45:02 +0300

    Changed main.py to use file_io.py

--- file_io.py
+++ file_io.py
@@ -2,8 +2,8 @@ FILE_NAME="input.txt"
 
 def read_greeting():
     greeting_file=open(FILE_NAME, 'r')
-    _content=greeting_file.read()
-    return _content
+    content=greeting_file.read()
+    return content
 
 if __name__=='__main__':
     print(read_greeting())
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 import module as m
+import file_io as fio
 print(f'{m.val}')
+print(f'{fio.read_greeting()}')
