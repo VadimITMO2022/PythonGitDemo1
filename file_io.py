2f35c2c735695f8dc47f916a2708f2885dc8bd0d
Vadim Varshavskiy <vadim_varshavskiy@yahoo.com>
2026-03-26 23:30:55 +0300

    Accidentally deleted and commited file file_io.py

--- file_io.py
+++
@@ -1,9 +0,0 @@
-FILE_NAME="input.txt"
-
-def read_greeting():
-    greeting_file=open(FILE_NAME, 'r')
-    content=greeting_file.read()
-    return content
-
-if __name__=='__main__':
-    print(read_greeting())
